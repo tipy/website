d8ca6883601225d66de2bfb2e3cefa092f280ac3
Gustavo Isensee <gustavo.isensee@gmail.com>
2022-06-27 14:28:42 +0200

    feat: add tipy locale lib for translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import tipyLogo from './assets/tipy-logo.png';
 import Label from './components/Label';
+import { t } from '@tipy/locale';
+import { useEffect } from 'react';
 
 const App = () => (
   <div className='w-full h-full max-w-md flex flex-col justify-center items-center'>
     <img src={tipyLogo} alt='Tipy logo' className='w-10 mb-4' />
-    <h2 className='text-center mb-4 font-mono'>
-      Tipy provides a set of useful libs to help developers on their daily basis
-      tasks.
-    </h2>
+    <h2 className='text-center mb-4 font-mono'>{t('description')}</h2>
 
     <div className='flex flex-row flex-wrap'>
       <Label color='indigo' name='website' />
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { init } from '@tipy/locale';
+import en from './assets/locales/en.json';
 import App from './App'
 import './index.css'
+
+init({ resource: en });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
